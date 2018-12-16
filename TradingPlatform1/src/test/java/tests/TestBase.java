7c9a2af836b0dc98b3bd8877c279262605bab483
SlavaPihidko <slava17puh@gmail.com>
2018-12-16 20:59:43 +0200

    начеркан коннект и выбор данных с ДБ

--- TradingPlatform1/src/test/java/tests/TestBase.java
+++ TradingPlatform1/src/test/java/tests/TestBase.java
@@ -4,6 +4,8 @@ import appmanager.ApplicationManager;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.sql.*;
+
 public class TestBase {
 
   protected final ApplicationManager app = new ApplicationManager();
@@ -16,6 +18,40 @@ public class TestBase {
   @AfterMethod
   public void teardown() {
     app.stop();
+  }
+
+
+  public void connToDB() {
+
+    String userName;
+    String password;
+    String dbURL;
+    Connection conn;
+
+    userName = "root";
+    password = "qwerty";
+    dbURL = "jdbc:mysql://146.71.78.211:3306?autoReconnect=true&useSSL=false";
+
+    try {
+      conn = DriverManager.getConnection(dbURL, userName, password);
+
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery("SELECT id FROM puredex.users where id=10;");
+
+      System.out.println(rs);
+
+
+      rs.close();
+      st.close();
+      conn.close();
+      // Do something with the Connection
+    } catch (SQLException ex) {
+
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+    }
   }
 
 }
