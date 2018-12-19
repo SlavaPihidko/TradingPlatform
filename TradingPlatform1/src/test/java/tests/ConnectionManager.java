fd5314ed4fb8a8ae6d7f4913e63cf3682dd05e39
SlavaPihidko <slava17puh@gmail.com>
2018-12-19 02:14:36 +0200

    успешный конект ssh

--- TradingPlatform1/src/test/java/tests/ConnectionManager.java
+++ TradingPlatform1/src/test/java/tests/ConnectionManager.java
@@ -5,9 +5,7 @@ import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class ConnectionManager {
 
@@ -23,27 +21,25 @@ public class ConnectionManager {
     int sshPort = 22;
     String sqlIp = "127.0.0.1";
     int sqlPort = 3306;
+    int lPort = 4321;
 
 
     if (useSSH) {
       JSch jSch = new JSch();
       try {
-        this.session = jSch.getSession("root",
-                "209.182.216.247",
-                Integer.valueOf(22));
+        this.session = jSch.getSession("root", "209.182.216.247", 22);
         this.session.setPassword("pxKL37ZA=n3A");
         this.session.setConfig("StrictHostKeyChecking", "no");
         System.out.println("Establishing Connection...");
         this.session.connect();
-        int assinged_port = this.session.setPortForwardingL(sshPort, sqlIp, sqlPort);
+        int assinged_port = this.session.setPortForwardingL(lPort, sqlIp, sqlPort);
         System.out.println(sqlIp+":"+assinged_port+" -> "+sqlIp+":"+sqlPort);
       } catch (JSchException e) {
         e.printStackTrace();
       }
     }
 
-    var connectionString = String.format("jdbc:mysql://127.0.0.1:3036/coin4coin_db?autoReconnect=true&useSSL=false",
-            sqlIp, useSSH ? sshPort : sqlPort);
+    var connectionString = String.format("jdbc:mysql://localhost:4321?autoReconnect=true&useSSL=false");
 
     var user = "root";
     var password = "qwerty";
@@ -51,6 +47,12 @@ public class ConnectionManager {
     try {
       Class.forName("com.mysql.jdbc.Driver");
       con = DriverManager.getConnection(connectionString, user, password);
+
+      Statement st = con.createStatement();
+      ResultSet rs = st.executeQuery("SELECT id FROM coin4coin_db.users where id=262;");
+
+      System.out.println(rs);
+
     } catch (SQLException e) {
       e.printStackTrace();
     } catch (ClassNotFoundException e) {
