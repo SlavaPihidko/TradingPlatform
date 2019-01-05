9ccb66c8c1392dfbedfa8b3f5e12dba51cda69d7
SlavaPihidko <slava17puh@gmail.com>
2019-01-06 01:22:29 +0200

    добавлены еще раз файлы типа properties

--- TradingPlatform1/src/test/java/tests/TestBase.java
+++ TradingPlatform1/src/test/java/tests/TestBase.java
@@ -4,28 +4,24 @@ import apirequests.ApiManager;
 import appmanager.ApplicationManager;
 import connmanager.ConnectionManager;
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-
 import java.io.IOException;
 
 public class TestBase  {
-//BrowserType.CHROME  -Dbrowser=chrome
+
   protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   protected static final ConnectionManager cm = new ConnectionManager();
 
   protected static final ApiManager am = new ApiManager();
 
-  // Запуск Браузера если true
   @BeforeSuite
   public void setUp() throws IOException {
     app.init();
     am.dealWithApi();
   }
-  // Киллинг Браузера если true
+
   @AfterSuite
   public void tearDown() {
     app.stop();
