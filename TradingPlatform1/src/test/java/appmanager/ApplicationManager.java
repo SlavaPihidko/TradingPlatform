5af0c48ad16096772edf9cba4784a612c3d9a02f
SlavaPihidko <slava17puh@gmail.com>
2019-03-09 18:31:23 +0200

    добавлено условие выбора конфигурации с  selenium server и без

--- TradingPlatform1/src/test/java/appmanager/ApplicationManager.java
+++ TradingPlatform1/src/test/java/appmanager/ApplicationManager.java
@@ -9,12 +9,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -37,20 +39,26 @@ public class ApplicationManager {
     String targetWeb = System.getProperty("targetWeb", "localWeb");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", targetWeb))));
 
-    if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-      System.out.println("I am " + BrowserType.FIREFOX);
-      System.out.println("I am from system property " + System.getProperty("browser"));
-    } else if (browser.equals(BrowserType.CHROME)) {
-      System.out.println("I am " + BrowserType.CHROME);
-      ChromeOptions options = new ChromeOptions();
-      options.addArguments("start-fullscreen");
-//      DesiredCapabilities caps = new DesiredCapabilities();
-//      caps.setCapability(ChromeOptions.CAPABILITY, options);
-      wd = new ChromeDriver(options);
-      System.out.println( "Свойства браузера: \n" + ((ChromeDriver) wd).getCapabilities());
-    } else if (browser.equals(BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
+    if("".equals(properties.getProperty("selenium.server"))) {
+      if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+        System.out.println("I am " + BrowserType.FIREFOX);
+        System.out.println("I am from system property " + System.getProperty("browser"));
+      } else if (browser.equals(BrowserType.CHROME)) {
+        System.out.println("I am " + BrowserType.CHROME);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-fullscreen");
+//      DesiredCapabilities capabilities = new DesiredCapabilities();
+//      capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        wd = new ChromeDriver(options);
+        System.out.println("Свойства браузера: \n" + ((ChromeDriver) wd).getCapabilities());
+      } else if (browser.equals(BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      }
+    } else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
 
     wait = new WebDriverWait(wd, 20);
